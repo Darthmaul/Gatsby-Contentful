1e9bd2285ead5cd70cc94489a0265f7117d5a88a
Andrew Nolan <andrew_nolan17@yahoo.com>
2019-04-09 12:07:20 -0400

    Testing bootstrap for this guy

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,12 +4,11 @@ import Img from 'gatsby-image'
 import styles from './hero.module.css'
 
 export default ({ data }) => (
-  <div className={styles.hero}>
-    <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
+  <div class="jumbotron">
+      <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} />
+      <h3 class="display-4">{data.name}</h3>
+      <p class="lead">{data.title}</p>
+      <hr class="my-4" />
       <p>{data.shortBio.shortBio}</p>
-    </div>
   </div>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import base from './base.css'
+import "./bootstrap.min.css"
 import Container from './container'
 import Navigation from './navigation'
 
