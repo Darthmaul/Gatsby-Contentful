cc1001f1519b0d4246699ba4532dfedc1b974b86
Andrew Nolan <andrew_nolan17@yahoo.com>
2019-04-11 10:43:48 -0400

    Small tweaks for css

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Img from 'gatsby-image'
 
-import styles from './hero.module.css'
 
 export default ({ data }) => (
   // Main jumbotron for a primary marketing message or call to action
@@ -9,7 +8,7 @@ export default ({ data }) => (
         <div class="container">
             <h1>Welcome to the Virtual Team Toolbox</h1>
             <h2>Helping teams <u>work</u></h2>
-            <p><a class="btn btn-success btn-lg" href="toolbox.html">Get Started &raquo;</a></p>
+            <p><a class="btn btn-success btn-lg" href="/toolbox">Get Started &raquo;</a></p>
         </div>
     </div>
 )
